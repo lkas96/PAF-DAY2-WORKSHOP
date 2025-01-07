40fc2837bdd80a72f53ae636057132ea67a0b675
lkas96 <kwek_lawson@hotmail.com>
2025-01-07 12:22:53 +0800

    Added db creation script

---
+++ CreateDB.sql
@@ -0,0 +1,23 @@
+-- create the database
+-- drop if any exists
+-- select the database
+-- create the table
+
+drop database if exists workshop_two;
+
+create database workshop_two;
+
+use workshop_two;
+
+create table rsvp (
+    email varchar(64) not null,
+    phone int not null, 
+    confirmation_date date not null,
+    comments varchar(256),
+    name varchar(128) not null,
+    constraint PK_email primary key (email), -- make primary key email unique
+    constraint CHK_phone check(phone >= 80000000 and phone <= 9999999) -- check for 8 digits
+);
+
+-- now load thru command line client mysql
+-- source SCRIPT.sql
