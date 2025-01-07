39e5b85c5c094d9a7c04bb29761d3d237359dd6d
lkas96 <kwek_lawson@hotmail.com>
2025-01-07 12:11:46 +0800

    Spring Initializer + Glassfish Json P

---
+++ src/main/java/vttp/batch5/paf/Day2/Workshop/Day2WorkshopApplication.java
@@ -0,0 +1,13 @@
+package vttp.batch5.paf.Day2.Workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day2WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp/batch5/paf/Day2/Workshop/Day2WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.batch5.paf.Day2.Workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day2WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
